a9359bb639b79498c59456fea3e09999f47fe6ec
anzwkk <anam89737@gmail.com>
2024-12-27 20:05:39 +0300

    TryParse changed by Parse

--- DayOfTheWeek/Program.cs
+++ DayOfTheWeek/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Введіть номер дня тижня (1-7): ");
+        int weekNum = int.Parse(Console.ReadLine());
+
+        switch (weekNum)
+        {
+            case 1:
+                Console.WriteLine("День неділі - Понеділок");
+                break;
+            case 2:
+                Console.WriteLine("День неділі - Вівторок");
+                break;
+            case 3:
+                Console.WriteLine("День неділі - Середа");
+                break;
+            case 4:
+                Console.WriteLine("День неділі - Четверг");
+                break;
+            case 5:
+                Console.WriteLine("День неділі - П'ятниця");
+                break;
+            case 6:
+                Console.WriteLine("День неділі - Субота");
+                break;
+            case 7:
+                Console.WriteLine("День неділі - Неділя");
+                break;
+
+        }
+
+    }
+}
